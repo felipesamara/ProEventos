0f1519a0ab95e20f055b1391ea203d77cd7b3ebe
Felipe Samara <felipe.inf1@gmail.com>
2021-07-02 15:28:39 -0300

    Seçao 5: Angular e interpolações.

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,8 +8,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public imgTamanho: number =  50;
+  public imgLargura: number = 50;
+  public mostrarImg = true;
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
 
+  public get filtroLista(): string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter((evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 || evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1)
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -17,9 +35,17 @@ export class EventosComponent implements OnInit {
     this.getEventos()
   }
 
+  alterarImagem(){
+    this.mostrarImg = !this.mostrarImg;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response =>
+      {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error)
 
     );
